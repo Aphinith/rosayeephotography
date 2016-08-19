ef0c6125658dfc72ac0ee56900538e94bbbce2a2
Aralya Phinith <aphinith84@gmail.com>
2016-08-19 10:02:06 -0700

    fix index.js file to correctly show where search bar will be placed

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import SearchBar from './public/components/searchBar.js'
 
 class HelloWorld extends Component {
   render() {
     return (
-      <div>Hello World!</div>
+      <div>
+        <div>Hello World!</div>
+        <div>
+          <SearchBar />
+        </div>
+      </div>
     )
   }
 }
